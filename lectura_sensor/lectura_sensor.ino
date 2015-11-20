058ba3677d7c4f9100709cef8168dad7d1e81237
alejo7053 <alejo7053@hotmail.com>
2015-11-20 02:25:07 -0500

    agrego comentarios al codigo y tabulaciones

--- lectura_sensor/lectura_sensor.ino
+++ lectura_sensor/lectura_sensor.ino
@@ -1,36 +1,33 @@
 #include <Wire.h>
 //Direccion I2C de la MPU
 #define MPU 0x68
-#define GYRO_CONFIG 0x1B
+#define GYRO_CONFIG 0x1B //direccion registro de configuracion del giroscopio
 //MPU-6050 da los valores en enteros de 16 bits
 //valores en bruto
 int16_t GY;
 
-
 void setup() {
- Wire.begin();
- Wire.beginTransmission(MPU);
- Wire.write(0x6B);
- Wire.write(0);
- Wire.endTransmission(true);
- Wire.begin();
- Wire.beginTransmission(MPU);
- Wire.write(GYRO_CONFIG);
- Wire.write(3<<3);
- Wire.endTransmission(true);
- Serial.begin(9600);
+   Wire.begin();
+   Wire.beginTransmission(MPU);
+   Wire.write(0x6B);
+   Wire.write(0);
+   Wire.endTransmission(true);
+   Wire.begin();
+   Wire.beginTransmission(MPU);
+   Wire.write(GYRO_CONFIG); //configuracion del rango de las salidas del giroscopio
+   Wire.write(3<<3);
+   Wire.endTransmission(true);
+   Serial.begin(9600);
 }
 
 void loop() {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x45); //pedimos el registro 0x45-corresponde al giroscopio en Y
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,2,true);//apartir  del registro 0x45, pedimos el resto de registros
+  GY=Wire.read()<<8|Wire.read();
+  Serial.print("GY = "); 
+  Serial.println(GY);
   
-Wire.beginTransmission(MPU);
-Wire.write(0x45); //pedimos el registro 0x45-corresponde al giroscopio en Y
-Wire.endTransmission(false);
-Wire.requestFrom(MPU,0,true);//apartir  del registro 0x45, pedimos el resto de registros
-GY=Wire.read()<<8|Wire.read();
-Serial.print("GY = "); 
-Serial.println(GY);
-
   delay(10);
-
 }
